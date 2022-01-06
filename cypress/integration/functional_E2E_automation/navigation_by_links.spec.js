ed4001dabbab5dda969f717287657f37bdd1ed0a
artem1911 <artem.tarasov.11@protonmail.ch>
2022-01-06 11:52:52 +0100

    added method of managing cookies

--- cypress/integration/functional_E2E_automation/navigation_by_links.spec.js
+++ cypress/integration/functional_E2E_automation/navigation_by_links.spec.js
@@ -1,1 +1,15 @@
 /// <reference types="cypress" />
+
+import {navigation} from '../../page_objects/functional_E2E_automation/navigation';
+import {searchByGoogle}  from '../../page_objects/functional_E2E_automation/searchByGoogle';
+
+
+describe('Navigation by links', () => {
+    beforeEach(() => {  
+        navigation.navigate();      
+    })
+
+    it('Should perform a search for "pxr legal" by google and search results displayed', () => {
+        
+    })
+})
